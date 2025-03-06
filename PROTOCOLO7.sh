9aeeb6ccef4048ee4d5649d197766af4389797ea
Gabriel ✪ <64110393+gabrielkelzer@users.noreply.github.com>
2025-03-06 19:53:10 -0300

    Update PROTOCOLO7.sh

--- PROTOCOLO7.sh
+++ PROTOCOLO7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "\033[1;32mParabéns o Software BrazilWing V2 VIP FREE está sem bugs no momento.\033[0m"
+echo -e "\033[1;32mParabéns o Software BrazilWing V3 VIP FREE está sem bugs no momento.\033[0m"
 sleep 5
 cd
 
